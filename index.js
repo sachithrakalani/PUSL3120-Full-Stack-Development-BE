a89dae611f2d3b7dbd831df14b096a316e713c66
sachithrakalani <kalanisathyani@gmail.com>
2024-12-01 22:25:41 +0530

    Create Server for Cinema booking system

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const express = require ('express');
+const app = express();
+
+
+//Create Server
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
+
+
+//Create Responce
+app.get("/",(req,res)=>[
+    res.send('Hello Node')
+]);
