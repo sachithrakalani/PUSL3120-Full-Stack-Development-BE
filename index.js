f6a644d1ed0d96d0e4fd3f78f4c33d01956f0b4b
sachithrakalani <kalanisathyani@gmail.com>
2024-12-02 14:57:19 +0530

    Modify above code

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const express = require ('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-
-//Create Server
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
-
-
 //Create Responce
-app.get("/",(req,res)=>[
-    res.send('Hello Node')
-]);
+app.get("/", (req, res) => [res.send("Hello Node API")]);
 
 //Connect the Database
-mongoose.connect("mongodb+srv://kalanisathyangi:Fmm8ncu3xBlRdBig@cinema-booking-system-d.1ahkq.mongodb.net/Cinema-Booking-System?retryWrites=true&w=majority&appName=Cinema-booking-system-DB")
-.then(()=>{
-    console.log("Connected to the Database!!")
-})
-.catch(()=>{
-    console.log("Connection Failed!!")
-})
+mongoose
+  .connect(
+    "mongodb+srv://kalanisathyangi:Fmm8ncu3xBlRdBig@cinema-booking-system-d.1ahkq.mongodb.net/Cinema-Booking-System?retryWrites=true&w=majority&appName=Cinema-booking-system-DB"
+  )
+  .then(() => {
+    console.log("Connected to the Database!!");
+    //Create Server
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection Failed!!");
+  });
