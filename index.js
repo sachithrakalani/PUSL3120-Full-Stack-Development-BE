573edb32aff2950c7c90eed64cda1c2b639fe3a3
sachithrakalani <kalanisathyani@gmail.com>
2024-12-04 10:27:55 +0530

    Add try and catch block

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Moives = require('./models/moive.model');
 
 const app = express();
 
@@ -10,9 +11,13 @@ app.get("/", (req, res) => [res.send("Hello Node API")]);
 app.use(express.json());
 
 //Add Moives
-app.post('/api/moives',(req,res)=>{
-    console.log(req.body);
-    res.send(req.body);
+app.post('/api/moives',async (req,res)=>{
+    try{
+        const moives = await Moives.create(req.body);
+        res.status(200).json(moives);
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
 });
 
 //Connect the Database
