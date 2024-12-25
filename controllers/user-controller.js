72e189b8c363ed429a1515d2f63e5be4dd56de33
sachithrakalani <kalanisathyani@gmail.com>
2024-12-25 12:27:55 +0530

    Add endpoint to login user with error handling

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -76,16 +76,45 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-    const id = req.params.id;
-    let user;
-  
-    try {
-      user = await User.findByIdAndDelete(id);
-    } catch (error) {
-      return console.log(error);
-    }
-    if (!user) {
-      return res.status(500).json({ message: "Something Went Wrong" });
-    }
-    return res.status(200).json({ message: "Deleted Successfully" });
-  };
+  const id = req.params.id;
+  let user;
+
+  try {
+    user = await User.findByIdAndDelete(id);
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!user) {
+    return res.status(500).json({ message: "Something Went Wrong" });
+  }
+  return res.status(200).json({ message: "Deleted Successfully" });
+};
+
+export const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || email.trim() === "" || !password || password.trim() === "") {
+    return res.status(422).json({ message: "Inavalid Inputs" });
+  }
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    return console.log(error);
+  }
+
+  if (!existingUser) {
+    return res
+      .status(404)
+      .json({ message: "Unable to find user from this ID" });
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Incorrect Password" });
+  }
+  return res.status(200).json({ message: "Login Successfully" });
+};
