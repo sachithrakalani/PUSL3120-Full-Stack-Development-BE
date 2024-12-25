64e087167b8ae5aa3fc7ef3cf21f8db559f7ec49
sachithrakalani <kalanisathyani@gmail.com>
2024-12-25 10:37:35 +0530

    add user update endpoint with validation and hashed password handling

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -33,7 +33,7 @@ export const signup = async (req, res, next) => {
   const hashedPassword = bcrypt.hashSync(password);
   let user;
   try {
-    user = new User({ username, email, password:hashedPassword });
+    user = new User({ username, email, password: hashedPassword });
     user = await user.save();
   } catch (error) {
     console.log(error);
@@ -43,4 +43,34 @@ export const signup = async (req, res, next) => {
     return res.status(500).json({ message: "Unexpected Error Occured" });
   }
   return res.status(201).json({ user });
+};
+
+export const updateUser = async (req, res, next) => {
+  const id = req.params.id;
+  const { name, email, password } = req.body;
+  if (
+    !name ||
+    name.trim() === "" ||
+    !email ||
+    email.trim() === "" ||
+    !password ||
+    password.trim() === ""
+  ) {
+    return res.status(422).json({ message: "Invalid Inputs" });
+  }
+  const hashedPassword = bcrypt.hashSync(password);
+  let user;
+  try {
+    user = await User.findByIdAndUpdate(id, {
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!user) {
+    return res.status(500).json({ message: "Something Went Worng" });
+  }
+  res.status(200).json({ message: "Updated Sucessfully" });
 };
