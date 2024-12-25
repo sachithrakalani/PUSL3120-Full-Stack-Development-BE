88d8be458dc0a8e4f0f338b145ac08beb4e9bbe8
sachithrakalani <kalanisathyani@gmail.com>
2024-12-25 09:56:54 +0530

    Add API endpoint for user signup with input validation and error handling

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,3 +15,29 @@ export const getAllUsers = async (req, res, next) => {
   }
   return res.status(200).json({ users });
 };
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    username.trim() === "" ||
+    !email ||
+    email.trim() === "" ||
+    !password ||
+    password.trim() === ""
+  ) {
+    return res.status(422).json({ message: "Invalid Inputs" });
+  }
+  let user;
+  try {
+    user = new User({ username, email, password });
+    user = user.save();
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+  if (!user) {
+    return res.status(500).json({ message: "Unexpected Error Occured" });
+  }
+  return res.status(201).json({ user });
+};
