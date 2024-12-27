a23aa0b646f147760cf91b2f0788faedd00a952b
sachithrakalani <kalanisathyani@gmail.com>
2024-12-27 20:46:52 +0530

    Implement API endpoint to delete booking by ID

--- routes/booking-routes.js
+++ routes/booking-routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getBookingById, newBooking } from "../controllers/booking-controller.js";
+import { deleteBooking, getBookingById, newBooking } from "../controllers/booking-controller.js";
 
 const bookingsRouter = express.Router();
 
 bookingsRouter.post("/", newBooking);
 bookingsRouter.get("/:id", getBookingById);
+bookingsRouter.delete(":id", deleteBooking);
 
 export default bookingsRouter;
